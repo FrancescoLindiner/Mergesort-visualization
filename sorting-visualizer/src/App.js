6d526d72594af3b39e788adca8e3db7ab9ba8db0
Francesco Lindiner <71122014+FrancescoLindiner@users.noreply.github.com>
2021-12-19 15:04:09 +0100

    Added the project

---
+++ sorting-visualizer/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+
+function App() {
+    return ( <
+        div >
+        <
+        SortingVisualizer > < /SortingVisualizer> < /
+        div >
+    );
+}
+
+export default App;
